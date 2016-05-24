6d89bd9e9e8e180a549e021b4e95666f607a7001
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-05-24 18:44:56 +0700

    running percobaan1.model

--- src/com/github/fawwaz/Thesis.java
+++ src/com/github/fawwaz/Thesis.java
@@ -5,6 +5,9 @@
  */
 package com.github.fawwaz;
 
+import com.github.fawwaz.util.Tester;
+import com.github.fawwaz.util.Trainer;
+
 /**
  *
  * @author Asus
@@ -16,7 +19,36 @@ public class Thesis {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        boolean isTraining = false;
+        boolean isTraining = false; // untuk training
+        boolean isTesting = true; // untuk labelling (prediction) , evaluating beda lagi.. (ngitung cross validation..)
+        Integer fold_num = 1;
+        Integer how_many_fold = 4;
+        String DB_USERNAME = "root";
+        String DB_PASSWORD = "";
+        String DB_URL = "jdbc:mysql://localhost/mytomcatapp";
+        
+        
+        if(isTraining && !isTesting){
+            String modelname = "percobaan1.model"; // to be writen or read
+            Trainer trainer = new Trainer(modelname,DB_USERNAME,DB_PASSWORD,DB_URL,fold_num,how_many_fold);
+            try{
+                trainer.train();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else if(isTesting && !isTraining){
+            String modelname = "percobaan1.model";
+            Tester tester = new Tester(modelname, fold_num, how_many_fold, Tester.SCREEN);
+            try{
+                tester.test();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else if(isTraining && isTesting){
+            System.out.println("For security reason, training and testing directly is unsupported yet");
+        }else{
+            System.out.println("Nothing to do here !");
+        }
     }
     
 }
